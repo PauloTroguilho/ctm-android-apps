26cc2de372e176f6c1ed649c5e9aa7adbfbe1e3c
cleber.t.moura <cleber.t.moura@gmail.com>
2011-10-14 21:48:05 +0000

    Finalizado calculo de recisão

--- CalculoFacil/src/br/com/calculofacil/CalculoFacilActivity.java
+++ CalculoFacil/src/br/com/calculofacil/CalculoFacilActivity.java
@@ -1,13 +1,21 @@
 package br.com.calculofacil;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.AdapterView.OnItemClickListener;
 
 public class CalculoFacilActivity extends AdMobActivity {
+	
+	private static final int DIALOG_AJUDA = 2;
 
 	private ListView listView;
 
@@ -33,6 +41,46 @@ public class CalculoFacilActivity extends AdMobActivity {
 		});
 
 		setAdView();
+	}
+	
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		switch (id) {
+			case DIALOG_AJUDA:
+				return new AlertDialog.Builder(CalculoFacilActivity.this)
+						.setIcon(R.drawable.icon_error).setTitle(getString(R.string.menu_ajuda))
+						.setMessage(getString(R.string.dialog_ajuda)).setPositiveButton(
+								R.string.alert_dialog_ok,
+								new DialogInterface.OnClickListener() {
+									public void onClick(DialogInterface dialog,
+											int whichButton) {
+										/* User clicked OK so do some stuff */
+									}
+								}).create();
+		}
+		return null;
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle item selection
+		switch (item.getItemId()) {
+			case R.id.ajuda:
+				showDialog(DIALOG_AJUDA);
+				return true;
+			case R.id.sair:
+				finish();
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
 	}
 
 }
