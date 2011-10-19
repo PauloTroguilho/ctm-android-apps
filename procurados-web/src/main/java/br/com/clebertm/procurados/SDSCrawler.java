ec807babfe9727fd5f27ab24398a9bae79636158
cleber.t.moura <cleber.t.moura@gmail.com>
2011-10-19 17:10:10 +0000

    Initial import.

---
+++ procurados-web/src/main/java/br/com/clebertm/procurados/SDSCrawler.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package br.com.clebertm.procurados;
+
+import java.io.InputStream;
+
+import org.webharvest.definition.ScraperConfiguration;
+import org.webharvest.runtime.Scraper;
+import org.xml.sax.InputSource;
+
+/**
+ * @author Cleber Moura <cleber.moura@tjpe.jus.br>
+ *
+ */
+public class SDSCrawler {
+	
+	private ScraperConfiguration config;
+	private Scraper scraper;
+
+	/**
+	 * 
+	 */
+	public SDSCrawler() {
+		InputStream fileConfig = getClass().getResourceAsStream("procurados_config.xml");
+		if (fileConfig == null) {
+			fileConfig = getClass().getResourceAsStream("/procurados_config.xml");
+		}
+		config = new ScraperConfiguration(new InputSource(fileConfig));
+		String workdir = System.getenv("PROCURADOS_WORKDIR");
+		workdir = "D:\\sds_workdir";
+		scraper = new Scraper(config, workdir);
+		scraper.setDebug(true); 
+		scraper.execute();
+	}
+	
+	public static void main(String[] args) {
+		SDSCrawler crawler = new SDSCrawler();
+	}
+
+}
