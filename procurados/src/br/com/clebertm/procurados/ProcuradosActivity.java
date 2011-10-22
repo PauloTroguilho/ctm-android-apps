7b5761243f478e9bc6306f58eb07eb4a5c61fad3
cleber.t.moura <cleber.t.moura@gmail.com>
2011-10-22 22:45:33 +0000

    Initial import.

---
+++ procurados/src/br/com/clebertm/procurados/ProcuradosActivity.java
@@ -0,0 +1,15 @@
+package br.com.clebertm.procurados;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProcuradosActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detalhes);
+        
+        
+    }
+}
